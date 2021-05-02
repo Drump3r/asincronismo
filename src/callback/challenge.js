20d9c33e9aebb8798de132fcc8b14825668f353c
Brian <jacklevybrsg@gmail.com>
2021-05-02 09:38:40 -0500

    Implementando Promesas

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -3,7 +3,7 @@ const API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', url_api, true);
+    xhttp.open('GET', url_api);
     xhttp.onreadystatechange = (event) => {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
