fd4d623808b0ed392c7f09276c4674649f61e59c
Brian <jacklevybrsg@gmail.com>
2021-05-01 22:44:09 -0500

    Múltiples Peticiones a un API con Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,17 +1,46 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
-    let xhttp = new XMLHttpRequest()
-    xhttp.open('GET', url, true)
-    xhttp.onreadystatechange = function(event) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (event) => {
         if (xhttp.readyState === 4) {
-            if (xhtp.status === 200) {
+            if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText))
             } else {
-                const error = new Error(`Error ${url_api}`)
+                let error = new Error('Error '+url_api)
                 return callback(error, null)
             }
         }
     }
     xhttp.send()
 }
+
+fetchData(API, (error1, data1) => {
+    if (error1) return console.error(error1);
+    
+    fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
+        if (error2) return console.error(error2);
+
+        fetchData(data2.origin.url, function(error3, data3){
+            if (error3) {
+                return console.error(error3);
+            }
+            console.log(data1.info.count)
+            console.log(data2.name)
+            console.log(data3.dimension) 
+
+            // rutas de las peticiones en orden
+            console.log(API);
+            console.log(API + data1.results[0].id); 
+            console.log(data2.origin.url); 
+        })
+    })
+}) 
+
+
+
+
+
+
